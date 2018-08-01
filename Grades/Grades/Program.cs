635fe21263cfcec51ad92d652f62d59ec0bcfe9b
Zishan Qureshi <zishan.qureshi@gmail.com>
2018-08-01 22:39:21 +0100

    C# Studying
    C# Studying.  Initial commit.

---
+++ Grades/Grades/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);   // f indifcates it's a float.  Otherwise it give error 'cannot convert from double to float'.
+
+        }
+    }
+}
