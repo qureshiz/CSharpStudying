fceeaf515008e54b27eaf55728ee9b1dae802739
Zishan Qureshi <zishan.qureshi@gmail.com>
2018-09-17 20:55:43 +0100

    Reference Types Testing
    Added unit test for Reference Types testing.

---
+++ Grades/Grades.Tests/Types/ReferenceTypeTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Grades.Tests.Types
+{
+    [TestClass]
+    public class ReferenceTypeTests
+    {
+        [TestMethod]
+        public void VariablesHoldAReference()
+        {
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+            g1.Name = "Zishan";
+            g2.Name = "Dave";
+            Assert.AreEqual(g1.Name, g2.Name);
+        }
+
+    }
+}
--- Grades/Grades/GradeBook.cs
+++ Grades/Grades/GradeBook.cs
@@ -45,5 +45,6 @@ namespace Grades
         private List<float> grades; // list is not to be available outside of the class.  Without the private keyword, the list would still be private but using the private keyword makes it clear.
         public static float MinimumGrade = 0; // A statis member of a class can be invoked without the new keyword.
         public static float MaximumGrade = 100; //Interestinglyu, this is reference in the class, not the instanct of the class i.e. GradeBook.MinimumGrade not book.MinimumGrade which doesn't exist.
+        public string Name;
     }
 }
