4dc8b2daf5738422cb27ff75569339394f5027e2
Zishan Qureshi <zishan.qureshi@gmail.com>
2018-09-24 22:16:34 +0100

    Utilities
    Read from text file.

---
+++ Grades/Utilities/Utilities.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace Utilities
+{
+    class ReadFromFile
+    {
+        static void Main()
+        {
+
+            string text = File.ReadAllText("c:\BreachedIncidents.txt");
+        }
+    }
+}
